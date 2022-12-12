b837906c37c07be8f23150a61afaa5cad88069b3
Adam Schønemann <adamschoenemann@gmail.com>
2022-12-12 19:34:09 +0100

    update process-pdfs script to default to 102x162

--- process-pdfs.sh
+++ process-pdfs.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 readonly script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $script_dir
-npm start -- "$@"
+npm start -- to-102x162 $@
 firefox result.pdf
